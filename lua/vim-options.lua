8a9982540d87fcd38e7cbd840d3016181edc1864
Jack <jackwong2332@gmail.com>
2024-02-10 15:35:50 +0000

    changed json

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,6 +7,12 @@ vim.cmd("set relativenumber")
 vim.cmd("set nowrap")
 vim.cmd("set conceallevel=2")
 vim.cmd("set termguicolors")
+vim.cmd("set wildmode=longest,list,full")
+vim.cmd("set wildmenu")
+vim.cmd('set signcolumn=yes')
+vim.cmd('set cursorline')
+vim.cmd('set colorcolumn=80')
+vim.cmd("set clipboard=unnamedplus")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ','
@@ -14,3 +20,7 @@ vim.g.maplocalleader = ','
 vim.keymap.set('n', '<Leader>el', ':norm A;<CR>')
 vim.keymap.set('n', '<Leader>noh', ':noh<CR>')
 
+-- illuminate
+-- navigator
+-- gitsigns
+-- noice.nvim
